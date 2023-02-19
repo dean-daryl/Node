475847df3e0942fb29ab8279c76f8634a4eab6b4
Dean Daryl MURENZI <56021941+dean-daryl@users.noreply.github.com>
2023-02-19 14:36:41 +0200

    Fixed Integration

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ export const PORT = process.env.PORT || 3000;
 export const app = express();
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'content-type');
+
   next();
 });
 app.use(json());
